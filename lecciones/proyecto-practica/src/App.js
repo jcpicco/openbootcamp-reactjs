df0f0663f031d1731983ccca041f93fb474086fb
jcpicco <jcpiccodev@gmail.com>
2023-05-17 13:24:57 +0200

    Structural changes

--- lecciones/proyecto-practica/src/App.js
+++ lecciones/proyecto-practica/src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
 import GreetingClass from './components/pure/greetingClass';
 import GreetingFunction from './components/pure/greetingFunction';
+import TaskListComponent from './components/container/TaskListComponent';
+/* eslint-enable */
 
 function App() {
   return (
@@ -9,10 +12,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {/* Componente propio greeting.jsx con propiedades*/}
+          {/* Componente propio greeting.jsx con propiedades */}
           {/* <GreetingClass name="Juanma"></Greeting> */}
-          {/* Componente propio greetingFunction.jsx con propiedades*/}
-          <GreetingFunction name="Juanma"></GreetingFunction>
+          {/* Componente propio greetingFunction.jsx con propiedades */}
+          {/* <GreetingFunction name="Juanma"></GreetingFunction> */}
+          {/* Componente lista de tasks */}
+          <TaskListComponent></TaskListComponent>
         </p>
       </header>
     </div>
---
+++ lecciones/proyecto-practica/src/components/container/TaskListComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Task } from '../../models/task.class';
+import { LEVELS } from '../../models/levels.enum';
+import TaskComponent from '../pure/taskComponent';
+
+
+const TaskListComponent = () => {
+    const defaultTask = new Task("Ejemplo", "Descripción por defecto", false, LEVELS.NORMAL);
+    const changeState = (id) => {
+        console.log("TODO: Cambiar estado de una tarea");
+    }
+
+    
+    return (
+        <div>
+            <div>
+                <h1>Tus tasks:</h1>
+            </div>
+            {/* TODO: Aplicar un For/Map para renderizar una lista de tasks */}
+            <TaskComponent task={ defaultTask }></TaskComponent>
+        </div>
+    );
+};
+
+
+export default TaskListComponent;
