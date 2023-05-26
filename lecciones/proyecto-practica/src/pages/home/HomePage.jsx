6be8109031c49103966a20927756b8104584f5b9
jcpicco <jcpiccodev@gmail.com>
2023-05-26 10:52:24 +0200

    11. Estructuración

---
+++ lecciones/proyecto-practica/src/pages/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <h2>Access to your tasks</h2>
+        </div>
+    );
+}
+
+
+export default HomePage;
---
+++ lecciones/proyecto-practica/src/pages/tasks/TasksPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskListComponent from '../../components/container/taskListComponent'
+
+const TasksPage = () => {
+    return (
+        <div>
+            <TaskListComponent></TaskListComponent>
+        </div>
+    );
+}
+
+
+export default TasksPage;
